c770b569b3f6407ab1b9504a50ebcf6abb1fcf33
msrpcoder <mangu.s.rajpurohit@gmail.com>
2021-08-14 21:11:45 +0530

    Parameterized script params.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 """
 Module level docstring.
 """
+import os
+import time
 
 
 class Calc:
@@ -35,12 +37,18 @@ class Calc:
         return self.param1 - self.param2
 
 
-def start_app():
+def start_app(no_of_times, sleep_time):
     '''start_app() docstring'''
-    return {
-        'add': Calc('add', 21, 38).calculate(),
-        'minus': Calc('minus', 4, 2).calculate()
-    }
+    for _ in range(no_of_times):
+        print({
+            'add': Calc('add', 21, 38).calculate(),
+            'minus': Calc('minus', 4, 2).calculate()
+        })
+        time.sleep(sleep_time)
+
+
+NO_OF_TIMES = int(os.environ.get('TIMES', 5))
+SLEEP_TIME = int(os.environ.get('SLEEP_TIME', 5))
 
 if __name__ == '__main__':
-    print(start_app())
+    start_app(NO_OF_TIMES, SLEEP_TIME)
