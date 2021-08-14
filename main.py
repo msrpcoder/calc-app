ba85559b89f3646cbb6be409a1ee9f9304545f04
msrpcoder <mangu.s.rajpurohit@gmail.com>
2021-08-14 20:04:19 +0530

    Fixes linting errors.

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
-class Calc(object):
-  def __init__(self, operator, param1, param2):
-    self.operator = operator
-    self.param1 = param1
-    self.param2 = param2
+"""
+Module level docstring.
+"""
 
-  def calculate(self):
-    method = getattr(self, self.operator, None)
-    if not method:
-      raise Exception(f'Invalid operator: {operator}')
-      
-    return method()
-  
-  def add(self):
-    return self.param1 + self.param2
-  
-  def minus(self):
-    return self.param1 - self.param2
+
+class Calc:
+    '''class-level docstring.'''
+
+    def __init__(self, operator, param1, param2):
+        '''
+        constructor method
+
+        :param operator <operator docs>
+        :param param1 <param1 docs>
+        :param param2 <param2 docs>
+        '''
+        self.operator = operator
+        self.param1 = param1
+        self.param2 = param2
+
+    def calculate(self):
+        '''calculate docstring'''
+        method = getattr(self, self.operator, None)
+        if not method:
+            raise Exception(f'Invalid operator: {self.operator}')
+
+        return method()
+
+    def add(self):
+        '''add docstring'''
+        return self.param1 + self.param2
+
+    def minus(self):
+        '''minus docstring'''
+        return self.param1 - self.param2
+
 
 def start_app():
-  return {
-    'add': Calc('add', 21, 38).calculate(),
-    'minus': Calc('minus', 4, 2).calculate()
-  }
-    
+    '''start_app() docstring'''
+    return {
+        'add': Calc('add', 21, 38).calculate(),
+        'minus': Calc('minus', 4, 2).calculate()
+    }
+
 if __name__ == '__main__':
-  print (start_app())
+    print(start_app())
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,14 @@
+'''Module level docstring.'''
 import unittest
 import main
 
-class MainTest(unittest.TestCase):
-  def test_constructor(self):
-    obj = main.Calc('add', 1, 2)
-    self.assertEqual(obj.operator, 'add')
-    self.assertEqual(obj.param1, 1)
-    self.assertEqual(obj.param2, 2)
 
+class MainTest(unittest.TestCase):
+    '''class docstring.'''
+
+    def test_constructor(self):
+        '''test method.'''
+        obj = main.Calc('add', 1, 2)
+        self.assertEqual(obj.operator, 'add')
+        self.assertEqual(obj.param1, 1)
+        self.assertEqual(obj.param2, 2)
